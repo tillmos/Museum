fe2e495186149c39122b79421fb6da42aa9b0bd6
tillmos <titanilla.halmos@gmail.com>
2022-11-09 08:00:47 +0100

    adding models

--- app/View/Components/masik.blade.php
+++ app/View/Components/masik.blade.php
@@ -7,6 +7,6 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Hi bitch</h1>
+    <h1>other</h1>
 </body>
 </html>
--- resources/views/other.blade.php
+++ resources/views/other.blade.php
@@ -7,6 +7,6 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Hi bitch</h1>
+    <h1>hola</h1>
 </body>
 </html>
